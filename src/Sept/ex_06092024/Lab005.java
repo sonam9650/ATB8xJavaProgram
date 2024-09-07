319f0fb95a56457c844eccfe95a4efd9582fb1e9
sonam9650 <sonamverma890@gmail.com>
2024-09-07 15:01:50 +0530

    Add file

---
+++ src/Sept/ex_06092024/Lab005.java
@@ -0,0 +1,11 @@
+package Sept.ex_06092024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+    }
+}
---
+++ src/Sept/ex_06092024/Lab006.java
@@ -0,0 +1,15 @@
+//This is example of comment
+
+//This belongs to package sept 06092024
+package Sept.ex_06092024;
+//class is Lab006
+public class Lab006 {
+    //This is main function JVM start from here
+    public static void main(String[] args) {
+        /* Then printing
+        * hello world
+        * using print method
+         */
+        System.out.println("Hello World");
+    }
+}
