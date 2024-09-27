2a02fd2ea77c00d634d6375dac28a913b87394f3
sonam9650 <sonamverma890@gmail.com>
2024-09-27 19:09:53 +0530

    23-09-2024 all files

--- src/Sept/ex_23092024/Lab085_task_02_month_Selection.java
+++ src/Sept/ex_23092024/Lab085_task_02_month_Selection.java
@@ -1,6 +1,6 @@
 package Sept.ex_23092024;
 
-public class Lab085_task_02_month {
+public class Lab085_task_02_month_Selection {
     public static void main(String[] args) {
         // Write a program that takes an integer input (1-12) and
         // prints the name of the corresponding month using a switch statement.
